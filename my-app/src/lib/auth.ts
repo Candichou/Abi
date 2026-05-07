c07aaf5353bdca001662c8d50ea417e7376884a2
/ <candice.chevret@gmail.com>
2026-05-07 13:36:05 +0200

    core:add plugings cookies file auth.ts

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -1,10 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db } from "@/src/index";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or "mysql"
   }),
-  //... the rest of your config
+  plugings: [nextCookies()], //permet de sauvegarder les cookies better-auth dans l'appli next
 });
