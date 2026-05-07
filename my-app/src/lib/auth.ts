8cab8f03ba88c07c47afd7c63cf601dfa11dc21e
/ <candice.chevret@gmail.com>
2026-05-07 12:09:46 +0200

    chore: install drizzle + betterAuth

---
+++ my-app/src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@/src/index";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "pg" or "mysql"
+  }),
+  //... the rest of your config
+});
