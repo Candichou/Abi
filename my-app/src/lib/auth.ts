ece83199fb617798b938980015f20c0f77f26ba2
/ <candice.chevret@gmail.com>
2026-05-08 14:18:25 +0200

    add APIkey resend in file .env.local

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -2,12 +2,15 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db } from "@/src/index";
 import { nextCookies } from "better-auth/next-js";
+import { admin } from "better-auth/plugins";
+import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     usePlural: true, // indication à betterAtuh que mes tables sont au pluriel
   }),
   emailAndPassword: { enabled: true },
-  plugins: [nextCookies()], //permet de sauvegarder les cookies better-auth dans l'appli next
+  plugins: [nextCookies(), admin()], //permet de sauvegarder les cookies better-auth dans l'appli next
 });
