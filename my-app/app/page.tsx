405124a4d2a60864061766c8f52822e401fe1f11
/ <candice.chevret@gmail.com>
2026-05-12 11:14:20 +0200

    fix:page signIn

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,17 +1,3 @@
-import SignIn from "@/components/signIn";
-import Dashboard from "./dashboard/page";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <button
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={Dashboard}
-      >
-        Se connecter
-      </button>
-      <SignIn />
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-    </div>
-  );
+  return;
 }
