3f5067e289bf4461f319216c68912658a79daa84
/ <candice.chevret@gmail.com>
2026-05-21 11:18:19 +0200

    core: change name app in plugin resend

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,8 +22,8 @@ export const auth = betterAuth({
       await resend.emails.send({
         from: "onboarding@resend.dev",
         to: user.email,
-        subject: "Vérifiez votre adresse email — CareApp",
-        html: `<p>Bonjour,</p><a href="${url}">Vérifier mon email</a>`,
+        subject: "Vérification de votre email",
+        html: `<p>Bonjour,</p><a href="${url}">Vérifier mon email</a>`, // TODO: faire un mail de vérification plus UX
       });
     },
   },
