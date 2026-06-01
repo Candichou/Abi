af0652ff8ce3b2b3827b3b1614a68831bebbd86c
/ <candice.chevret@gmail.com>
2026-06-01 22:54:21 +0200

    core

--- components/signIn/SignInForm.tsx
+++ components/signIn/SignInForm.tsx
@@ -166,9 +166,9 @@ export default function SignInForm() {
       </div>
 
       {/* Footer éthique */}
-      <p className="text-center font-body text-xs text-forest/50 mt-2">
+      {/*      <p className="text-center font-body text-xs text-forest/50 mt-2">
         Sans traceur · sans CAPTCHA visuel · données minimales · hébergement UE
-      </p>
+      </p> */}
     </form>
   );
 }
